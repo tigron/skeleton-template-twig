4274a7bce17944556ac563d7cf6e6f9b1a314d1c
Gerry Demaret <gerry@tigron.be>
2015-08-25 14:13:46 +0200

    Enable the Twig "Debug" extension
    This should probably be made configurable?

--- lib/Skeleton/Template/Twig/Twig.php
+++ lib/Skeleton/Template/Twig/Twig.php
@@ -81,6 +81,7 @@ class Twig {
 
 		$this->twig->addExtension(new \Skeleton\Template\Twig\Extension\Common());
 		$this->twig->addExtension(new \Twig_Extension_StringLoader());
+		$this->twig->addExtension(new \Twig_Extension_Debug());
 		$this->twig->getExtension('core')->setNumberFormat(2, '.', '');
 	}
 
