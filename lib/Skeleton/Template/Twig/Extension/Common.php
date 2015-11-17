90d23cdd9b50c1066ccd769f8cd0e15852508340
Gerry Demaret <gerry@tigron.be>
2015-11-17 14:49:37 +0100

    Cleanup, add object_sort filter, rewrite filesize

--- lib/Skeleton/Template/Twig/Extension/Common.php
+++ lib/Skeleton/Template/Twig/Extension/Common.php
@@ -18,9 +18,9 @@ class Common extends \Twig_Extension {
 	 * @access public
 	 */
 	public function initRuntime(\Twig_Environment $environment) {
-        parent::initRuntime($environment);
-        $this->environment = $environment;
-    }
+		parent::initRuntime($environment);
+		$this->environment = $environment;
+	}
 
 	/**
 	 * Returns a list of globals
@@ -44,13 +44,13 @@ class Common extends \Twig_Extension {
 		return $globals;
 	}
 
-    /**
-     * Returns a list of filters
-     *
-     * @return array
-     */
-    public function getFilters() {
-        return [
+	/**
+	 * Returns a list of filters
+	 *
+	 * @return array
+	 */
+	public function getFilters() {
+		return [
 			new \Twig_SimpleFilter('print_r', [$this, 'print_r_filter'], ['is_safe' => ['html']]),
 			new \Twig_SimpleFilter('json_decode', [$this, 'json_decode_filter'], ['is_safe' => ['html']]),
 			new \Twig_SimpleFilter('serialize', [$this, 'serialize_filter'], ['is_safe' => ['html']]),
@@ -59,21 +59,18 @@ class Common extends \Twig_Extension {
 			new \Twig_SimpleFilter('datetime', [$this, 'datetime_filter'], ['needs_environment' => true, 'is_safe' => ['html']]),
 			new \Twig_SimpleFilter('filesize', [$this, 'filesize_filter'], ['needs_environment' => true, 'is_safe' => ['html']]),
 			new \Twig_SimpleFilter('rewrite', [$this, 'rewrite_filter'], ['needs_environment' => true, 'is_safe' => ['html']]),
-			new \Twig_SimpleFilter('markdown', [$this, 'markdown_filter'], ['needs_environment' => true, 'is_safe' => ['html']]),
-        ];
+			new \Twig_SimpleFilter('object_sort', [$this, 'object_sort_filter'], ['needs_environment' => true, 'is_safe' => ['html']]),
+		];
 	}
 
 	/**
-     * Returns a list of functions
-     *
-     * @return array
-     */
+	 * Returns a list of functions
+	 *
+	 * @return array
+	 */
 	public function getFunctions() {
 		return [
 			new \Twig_SimpleFunction('strpos', [$this, 'strpos_function'], ['is_safe' => ['html']]),
-			new \Twig_SimpleFunction('math_add', [$this, 'math_add_function'], ['is_safe' => ['html']]),
-			new \Twig_SimpleFunction('math_sub', [$this, 'math_sub_function'], ['is_safe' => ['html']]),
-			new \Twig_SimpleFunction('math_mul', [$this, 'math_mul_function'], ['is_safe' => ['html']]),
 		];
 	}
 
@@ -96,7 +93,7 @@ class Common extends \Twig_Extension {
 			$output .= '</pre>';
 		}
 
-	    return $output;
+		return $output;
 	}
 
 	/**
@@ -106,7 +103,7 @@ class Common extends \Twig_Extension {
 	 * @return string $output
 	 */
 	public function serialize_filter($value) {
-	    return serialize($value);
+		return serialize($value);
 	}
 
 	/**
@@ -155,40 +152,38 @@ class Common extends \Twig_Extension {
 	/**
 	 * Filesize filter
 	 *
-	 * @param int $filesize
+	 * @param int $filesize File size, in bytes
+	 * @param int $precision Round the output to $precision
+	 * @param string $decimal_mark Decimal mark to use
+	 * @param string $system System to use, can be "iec" (default) or "metric"
 	 * @return string $output
 	 */
-	public function filesize_filter(\Twig_Environment $env, $filesize) {
-		$new_filesize = $filesize / 1024;
-		if ($new_filesize < 1) {
-			return number_format($filesize, 2, '.', ' ') . 'b';
-		}
-		$filesize = $new_filesize;
+	public function filesize_filter(\Twig_Environment $env, $filesize, $precision = 2, $decimal_mark = '.', $system = 'iec') {
+		$iec_suffixes = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+		$metric_suffixes = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
-		$new_filesize = $filesize / 1024;
-		if ($new_filesize < 1) {
-			return number_format($filesize, 2, '.', ' ') . 'Kb';
-		}
-		$filesize = $new_filesize;
-
-		$new_filesize = $filesize / 1024;
-		if ($new_filesize < 1) {
-			return number_format($filesize, 2, '.', ' ') . 'Mb';
-		}
-		$filesize = $new_filesize;
-
-		$new_filesize = $filesize / 1024;
-		if ($new_filesize < 1) {
-			return number_format($filesize, 2, '.', ' ') . 'Gb';
-		}
-		$filesize = $new_filesize;
-
-		$new_filesize = $filesize / 1024;
-		if ($new_filesize < 1) {
-			return number_format($filesize, 2, '.', ' ') . 'Tb';
+		if ($system == 'iec') {
+			$unit = 1024;
+			$suffixes = $iec_suffixes;
+		} elseif ($system == 'metric') {
+			$unit = 1000;
+			$suffixes = $metric_suffixes;
 		} else {
-			return 'unknown';
+			throw new \Exception('System not supported');
 		}
+
+		$current_filesize = $filesize;
+		foreach ($suffixes as $suffix) {
+			$new_filesize = $current_filesize / $unit;
+
+			if ($new_filesize < 1) {
+				return number_format($current_filesize, $precision, $decimal_mark, ' ') . ' ' . $suffix;
+			}
+
+			$current_filesize = $new_filesize;
+		}
+
+		return $filesize;
 	}
 
 	/**
@@ -202,18 +197,55 @@ class Common extends \Twig_Extension {
 			return \Skeleton\Core\Util::rewrite_reverse($url);
 		}
 
-		return "foo";
+		return $url;
 	}
 
 	/**
-	 * Filter markdown
+	 * Filter object_sort
 	 *
-	 * @param string $content
+	 * @param array $objects Array containing the objects to be supported
+	 * @param string $property Property on which to sort the objects
+	 * @param string $direction Direction in which to sort the objects
+	 * @param string $type Type of sorting to apply, can be "auto" (default), "string" or "date"
 	 * @return string $output
 	 */
-	public function markdown_filter(\Twig_Environment $env, $content) {
-		// FIXME dummy filter for now
-		return $content;
+	public function object_sort(\Twig_Environment $env, $objects, $property, $direction = 'asc', $type = 'auto') {
+		usort($objects, function($a, $b) use ($property, $direction, $type) {
+			if (!is_object($property) AND isset($a->$property)) {
+				$property1 = $a->$property;
+				$property2 = $b->$property;
+			} elseif (is_callable([$a, $property])) {
+				$property1 = call_user_func_array([$a, $property], []);
+				$property2 = call_user_func_array([$a, $property], []);
+			} elseif (is_callable($property)) {
+				$property1 = $property($a);
+				$property2 = $property($b);
+			}
+
+			if (is_numeric($property1) AND is_numeric($property2) AND $type == 'auto') {
+				$type = 'int';
+			}
+
+			if ($type == 'string') {
+				$cmp = strcasecmp($property1, $property2);
+			} elseif ($type == 'date') {
+				if (strtotime($property1) > strtotime($property2)) {
+					$cmp = 1;
+				} else {
+					$cmp = -1;
+				}
+			} else {
+				$cmp = $property1 > $property2 ? 1 : -1;
+			}
+
+			if ($direction == 'desc') {
+				return -1*$cmp;
+			} else {
+				return $cmp;
+			}
+		});
+
+		return $objects;
 	}
 
 	/**
@@ -224,48 +256,15 @@ class Common extends \Twig_Extension {
 	 * @return mixed $output
 	 */
 	public function strpos_function($value, $to_search) {
-	    return strpos($value, $to_search);
+		return strpos($value, $to_search);
 	}
 
 	/**
-	 * Function math add
+	 * Name of this extension
 	 *
-	 * @param float $value
-	 * @param float $value2
-	 * @return float $sum
+	 * @return string
 	 */
-	public function math_add_function($value, $value2) {
-		return Util::math_add($value, $value2);
+	public function getName() {
+		return 'Common';
 	}
-
-	/**
-	 * Function math sub
-	 *
-	 * @param float $value
-	 * @param float $value2
-	 * @return float $diff
-	 */
-	public function math_sub_function($value, $value2) {
-		return Util::math_sub($value, $value2);
-	}
-
-	/**
-	 * Function math multiply
-	 *
-	 * @param float $value
-	 * @param float $value2
-	 * @return float $product
-	 */
-	public function math_mul_function($value, $value2) {
-		return Util::math_mul($value, $value2);
-	}
-
-    /**
-     * Name of this extension
-     *
-     * @return string
-     */
-    public function getName() {
-        return 'Common';
-    }
 }
