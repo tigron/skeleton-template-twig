cc0c25afe7de88be27882c78440a012734755aa4
tigron <tigron@aftersalesplatform.com>
2016-02-12 16:14:29 +0100

    fix get_class filter

--- lib/Skeleton/Template/Twig/Extension/Common.php
+++ lib/Skeleton/Template/Twig/Extension/Common.php
@@ -60,7 +60,7 @@ class Common extends \Twig_Extension {
 			new \Twig_SimpleFilter('filesize', [$this, 'filesize_filter'], ['needs_environment' => true, 'is_safe' => ['html']]),
 			new \Twig_SimpleFilter('rewrite', [$this, 'rewrite_filter'], ['needs_environment' => true, 'is_safe' => ['html']]),
 			new \Twig_SimpleFilter('object_sort', [$this, 'object_sort_filter'], ['needs_environment' => true, 'is_safe' => ['html']]),
-			new \Twig_SimpleFilter('get_class', [$this, 'get_class_filter'], ['needs_environment' => true, 'is_safe' => ['html']]),
+			new \Twig_SimpleFilter('get_class', [$this, 'get_class_filter'], ['is_safe' => ['html']]),
 		];
 	}
 
