3a3790e64f3ceef57564dbf263c80e67dece57ce
Dmytro Kharchenko <d.kharchenko@bvblogic.com>
2022-07-01 11:34:25 +0300

    DimX / use by default non case sensitive sorting /

--- lib/Skeleton/Template/Twig/Extension/Common.php
+++ lib/Skeleton/Template/Twig/Extension/Common.php
@@ -272,8 +272,8 @@ class Common extends \Twig\Extension\AbstractExtension {
 				$type = 'int';
 			}
 
-			if ($type == 'string') {
-				$cmp = strcasecmp($property1, $property2);
+			if ($type == 'int') {
+                $cmp = $property1 > $property2 ? 1 : -1;
 			} elseif ($type == 'date') {
 				if (strtotime($property1) > strtotime($property2)) {
 					$cmp = 1;
@@ -281,7 +281,7 @@ class Common extends \Twig\Extension\AbstractExtension {
 					$cmp = -1;
 				}
 			} else {
-				$cmp = $property1 > $property2 ? 1 : -1;
+                $cmp = strcasecmp($property1, $property2);
 			}
 
 			if ($direction == 'desc') {
